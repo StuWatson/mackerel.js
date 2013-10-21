3e685c054c9bae5113a4bfc0686ba7d638d181c7
StuWatson <stuart.watson@appsbroker.com>
2013-10-21 16:38:37 +0100

    Eating function and array

--- mackerel.js
+++ mackerel.js
@@ -1,5 +1,7 @@
 var Mackerel = (function(win, undefined){
   var Mackerel = {
+    eats: ["Small finfish", "Squid", "Pelagic Crustaceans", "Base"],
+    
     isFish: function(){
       return true;
     },
@@ -12,6 +14,10 @@ var Mackerel = (function(win, undefined){
     canBreathe: function(parameters) {
       if(parameters.isUnderWater) return true;
       return false;
+    }
+    eat: function(parameters) {
+      if(this.eats.indexOf(parameters)!=-1) return "Mackerel ate "+parameters;
+      return "Mackeral does not eat "+parameters;
     }
   };
   
